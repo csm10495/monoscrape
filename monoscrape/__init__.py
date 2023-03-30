2b452efe7cbd5ccca2fe7af5fea0742135b5f62d
Charles Machalow <csm10495@gmail.com>
2023-03-29 21:55:03 -0700

    lint

--- monoscrape/__init__.py
+++ monoscrape/__init__.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
-from functools import cache
-import requests
-import json
-from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from dataclasses import dataclass, asdict
-import pathlib
 
+import json
+import pathlib
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from dataclasses import asdict, dataclass
+from functools import cache
 from threading import Lock
 
+import requests
+from bs4 import BeautifulSoup
+
 _printLock = Lock()
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 
 @dataclass
@@ -18,6 +19,7 @@ class Item:
     """
     Represents a single item on monoprice.com
     """
+
     name: str
     price: float
     url: str
@@ -31,7 +33,9 @@ class Item:
         """
         Takes a list of items and dumps it as json to the given out_file
         """
-        pathlib.Path(out_file).write_text(json.dumps([asdict(i) for i in items], indent=4))
+        pathlib.Path(out_file).write_text(
+            json.dumps([asdict(i) for i in items], indent=4)
+        )
 
     @classmethod
     def load_item_list_from_file(cls, in_file: str) -> list[Item]:
@@ -40,41 +44,57 @@ class Item:
         data = pathlib.Path(in_file).read_text()
         return [Item(**i) for i in json.loads(data)]
 
+
 @cache
 def __fetch(product_id: int) -> Item | None:
     """
     Does a single fetch of info for a given product_id. Will return None, if the product isn't found.
     """
-    result = requests.get(f'https://www.monoprice.com/product?p_id={product_id}')
+    result = requests.get(f"https://www.monoprice.com/product?p_id={product_id}")
     if result.status_code != 200:
         return None
 
-    soup = BeautifulSoup(result.text, 'html.parser')
-    if 'The product you’re looking for is no longer available' in soup.text:
+    soup = BeautifulSoup(result.text, "html.parser")
+    if "The product you’re looking for is no longer available" in soup.text:
         return None
 
     try:
-        rating = float(soup.find('span', id='TTreviewSummaryAverageRating').text.strip().split('/')[0].strip())
+        rating = float(
+            soup.find("span", id="TTreviewSummaryAverageRating")
+            .text.strip()
+            .split("/")[0]
+            .strip()
+        )
     except:
         rating = 0
 
     try:
-        num_reviews = int(soup.find('div', class_='TTreviewCount').text.strip().split(' ')[0].strip())
+        num_reviews = int(
+            soup.find("div", class_="TTreviewCount").text.strip().split(" ")[0].strip()
+        )
     except:
         num_reviews = 0
 
     try:
-        ret = Item(name=soup.find('div', {'class': 'product-name'}).text.strip(),
-                    price=float(soup.find('span', {'class': 'sale-price'}).text.strip().replace('$', '').replace(',', '')),
-                    url=result.url,
-                    product_id=product_id,
-                    available=bool(soup.find('a', text='Add to Cart')),
-                    rating=rating,
-                    num_reviews=num_reviews)
+        ret = Item(
+            name=soup.find("div", {"class": "product-name"}).text.strip(),
+            price=float(
+                soup.find("span", {"class": "sale-price"})
+                .text.strip()
+                .replace("$", "")
+                .replace(",", "")
+            ),
+            url=result.url,
+            product_id=product_id,
+            available=bool(soup.find("a", text="Add to Cart")),
+            rating=rating,
+            num_reviews=num_reviews,
+        )
     except AttributeError:
         return None
 
     return ret
+
 
 def fetch(product_id: int) -> Item | None:
     """
@@ -89,12 +109,20 @@ def fetch(product_id: int) -> Item | None:
 
     return ret
 
+
 def fetch_x(starting_product_id: int, num_products: int, workers: int) -> list[Item]:
     """
     Fetches a range of products from starting_product_id to starting_product_id + num_products
     """
     with ProcessPoolExecutor(max_workers=workers) as pool:
-        return [a for a in pool.map(fetch, range(starting_product_id, starting_product_id + num_products)) if a]
+        return [
+            a
+            for a in pool.map(
+                fetch, range(starting_product_id, starting_product_id + num_products)
+            )
+            if a
+        ]
+
 
 def fetch_all(fetch_size: int, max_product_id: int, workers: int) -> list[Item]:
     """
@@ -106,9 +134,11 @@ def fetch_all(fetch_size: int, max_product_id: int, workers: int) -> list[Item]:
             if temp_results := fetch_x(i, min(fetch_size, max_product_id + 1), workers):
                 results.extend(temp_results)
             else:
-                print(f"No items found during fetch_x({i}, {min(fetch_size, max_product_id + 1)}).. stopping")
+                print(
+                    f"No items found during fetch_x({i}, {min(fetch_size, max_product_id + 1)}).. stopping"
+                )
                 break
     except KeyboardInterrupt:
-        print (" -- Keyboard Interrupt -- Stopping --")
+        print(" -- Keyboard Interrupt -- Stopping --")
 
     return results
